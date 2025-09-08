2969d74b224d8af442a7d2ecde47aeb9d8d4dab9
pedzero <pedro9669silva@gmail.com>
2025-09-07 22:59:50 -0300

    chore(routes): add users routes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+//router.post("/", UsersController.createUser);
+//router.post("/:id/groups", UsersController.assignUserToGroup);
+//router.get("/:id/organizations", UsersController.getUserOrganizations);
+
+export default router;
