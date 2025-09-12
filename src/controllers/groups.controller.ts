0baf3ec38d540cf1f2eae39798310d435ebc40a2
pedzero <pedro9669silva@gmail.com>
2025-09-11 22:48:57 -0300

    refactor(groups): enhance type safety in group controller methods

--- src/controllers/groups.controller.ts
+++ src/controllers/groups.controller.ts
@@ -1,31 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import * as GroupsService from "../services/groups.service";
-import { createGroupSchema } from "../dtos/group.dtos";
+import { CreateGroupDto, createGroupSchema, FindGroupDto, findGroupSchema } from "../dtos/group.dtos";
+import { Group } from "@prisma/client";
 
-export async function createGroup(request: Request, response: Response, next: NextFunction) {
+export async function createGroup(request: Request, response: Response, next: NextFunction): Promise<void> {
     try {
-        const dto = createGroupSchema.parse(request.body);
-        const group = await GroupsService.createGroup(dto);
+        const dto: CreateGroupDto = createGroupSchema.parse(request.body);
+        const group: Group = await GroupsService.createGroup(dto);
         response.status(201).json(group);
     } catch (error) {
         next(error);
     }
 }
 
-export async function getGroupAncestors(request: Request, response: Response, next: NextFunction) {
+export async function getGroupAncestors(request: Request, response: Response, next: NextFunction): Promise<void> {
     try {
-        const { id: groupId } = request.params;
-        const ancestors = await GroupsService.getGroupAncestors({ groupId });
+        const dto: FindGroupDto = findGroupSchema.parse(request.params);
+        const ancestors: Node[] = await GroupsService.getGroupAncestors(dto);
         response.status(200).json(ancestors);
     } catch (error) {
         next(error);
     }
 }
 
-export async function getGroupDescendants(request: Request, response: Response, next: NextFunction) {
+export async function getGroupDescendants(request: Request, response: Response, next: NextFunction): Promise<void> {
     try {
-        const { id: groupId } = request.params;
-        const descendants = await GroupsService.getGroupDescendants({ groupId });
+        const dto: FindGroupDto = findGroupSchema.parse(request.params);
+        const descendants: Node[] = await GroupsService.getGroupDescendants(dto);
         response.status(200).json(descendants);
     } catch (error) {
         next(error);
