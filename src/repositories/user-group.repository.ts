90909cababc2f654cdabebf9af92a650cdcd7f13
pedzero <pedro9669silva@gmail.com>
2025-09-11 23:16:03 -0300

    refactor(user-group): enhance type safety in user group repository methods

--- src/repositories/user-group.repository.ts
+++ src/repositories/user-group.repository.ts
@@ -1,10 +1,11 @@
 import { prisma } from "../infra/db/prisma";
 import { UserGroup } from "../models/user-group.model";
 import { UserGroupDto } from "../dtos/user-group.dtos";
+import { Group } from "@prisma/client";
 
 export async function findUnique(data: UserGroupDto): Promise<UserGroup | null> {
-    const userGroup = await prisma.userGroup.findUnique({
-        where: { 
+    const userGroup: UserGroup | null = await prisma.userGroup.findUnique({
+        where: {
             userId_groupId: {
                 userId: data.userId,
                 groupId: data.groupId,
@@ -16,12 +17,12 @@ export async function findUnique(data: UserGroupDto): Promise<UserGroup | null> 
         userId: userGroup.userId,
         groupId: userGroup.groupId,
     } : null;
-    
+
     return userGroupResult;
 }
 
 export async function addUserToGroup(data: UserGroupDto): Promise<UserGroup> {
-    const created = await prisma.userGroup.create({
+    const created: UserGroup | null = await prisma.userGroup.create({
         data: {
             userId: data.userId,
             groupId: data.groupId,
