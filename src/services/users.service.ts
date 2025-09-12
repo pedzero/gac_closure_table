ed35bf476ba82e4153f97efd6564724bbfd6d4e3
pedzero <pedro9669silva@gmail.com>
2025-09-11 23:14:28 -0300

    refactor(users): enhance type safety in user service methods

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -5,27 +5,31 @@ import * as GroupsRepository from "../repositories/groups.repository";
 import * as userGroupRepository from "../repositories/user-group.repository";
 import { ConflictError, NotFoundError } from "../utils/errors";
 import { UserOrganization } from "../models/organization.model";
+import { User } from "@prisma/client";
+import { Group } from "../models/group.model";
+import { UserGroup } from "../models/user-group.model";
 
-export async function createUser(data: CreateUserDto) {
-    const user = await UsersRepository.createUser(data);
+export async function createUser(data: CreateUserDto): Promise<User> {
+    const user: User = await UsersRepository.createUser(data);
+
     return user;
 }
 
 export async function addUserToGroup(data: UserGroupDto): Promise<void> {
     // check if user exists
-    const user = await UsersRepository.findUnique({ userId: data.userId });
+    const user: User | null = await UsersRepository.findUnique({ userId: data.userId });
     if (!user) {
         throw new NotFoundError(`User ${data.userId} not found`);
     }
 
     // check if group exists
-    const group = await GroupsRepository.findUnique({ groupId: data.groupId });
+    const group: Group | null = await GroupsRepository.findUnique({ groupId: data.groupId });
     if (!group) {
         throw new NotFoundError(`Group ${data.groupId} not found`);
     }
 
     // avoid duplicate entries
-    const exists = await userGroupRepository.findUnique(data);
+    const exists: UserGroup | null = await userGroupRepository.findUnique(data);
     if (exists) {
         throw new ConflictError(`User ${data.userId} is already in group ${data.groupId}`);
     }
@@ -35,7 +39,7 @@ export async function addUserToGroup(data: UserGroupDto): Promise<void> {
 
 export async function getUserOrganizations(data: FindUserDto): Promise<UserOrganization[]> {
     // check if user exists
-    const user = await UsersRepository.findUnique(data);
+    const user: User | null = await UsersRepository.findUnique(data);
     if (!user) {
         throw new NotFoundError(`User ${data.userId} not found`);
     }
