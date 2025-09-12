ee3bf38df91b53350a95970edfa40698be49c0a8
pedzero <pedro9669silva@gmail.com>
2025-09-11 22:54:03 -0300

    refactor(users): enhance type safety in user controller methods

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,26 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import * as UsersService from "../services/users.service";
-import { createUserSchema } from "../dtos/user.dtos";
-import { UserGroupSchema } from "../dtos/user-group.dtos";
+import { CreateUserDto, createUserSchema, FindUserDto, findUserSchema } from "../dtos/user.dtos";
+import { UserGroupDto, UserGroupSchema } from "../dtos/user-group.dtos";
+import { User } from "../models/user.model";
+import { UserOrganization } from "../models/organization.model";
 
-export async function createUser(request: Request, response: Response, next: NextFunction) {
+export async function createUser(request: Request, response: Response, next: NextFunction): Promise<void> {
     try {
-        const dto = createUserSchema.parse(request.body);
-        const user = await UsersService.createUser(dto);
+        const dto: CreateUserDto = createUserSchema.parse(request.body);
+        const user: User = await UsersService.createUser(dto);
         response.status(201).json(user);
     } catch (error) {
         next(error);
     }
 }
 
-export async function assignUserToGroup(request: Request, response: Response, next: NextFunction) {
+export async function assignUserToGroup(request: Request, response: Response, next: NextFunction): Promise<void> {
     try {
         const { id: userId } = request.params;
-        const data = {
+        const data: UserGroupDto = {
             ...request.body,
             userId,
         };
-        const dto = UserGroupSchema.parse(data);
+        const dto: UserGroupDto = UserGroupSchema.parse(data);
         await UsersService.addUserToGroup(dto);
         response.status(204).send();
     } catch (error) {
@@ -28,10 +30,10 @@ export async function assignUserToGroup(request: Request, response: Response, ne
     }
 }
 
-export async function getUserOrganizations(request: Request, response: Response, next: NextFunction) {
+export async function getUserOrganizations(request: Request, response: Response, next: NextFunction): Promise<void> {
     try {
-        const { id: userId } = request.params;
-        const organizations = await UsersService.getUserOrganizations({ userId });
+        const dto: FindUserDto = findUserSchema.parse(request.params);
+        const organizations: UserOrganization[] = await UsersService.getUserOrganizations(dto);
         response.status(200).json(organizations);
     } catch (error) {
         next(error);
